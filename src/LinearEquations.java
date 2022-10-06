433f93a170f06d1acc1d329f0f6fa33f4c5d4e2d
Uziel Gaeta <ugaeta@cpp.edu>
2022-10-06 10:44:10 -0700

    Finished user input method

--- src/LinearEquations.java
+++ src/LinearEquations.java
@@ -1,5 +1,7 @@
-import java.lang.reflect.Array;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.Stack;
 
 /**
@@ -10,23 +12,29 @@ import java.util.Stack;
 
 public class LinearEquations {
     public static void main(String[] args) {
+        double[][] matrix;
 
-        double[][] matrix = {{3, -13, 9, 3, -19},
-                            {-6, 4, 1, -18, -34},
-                            {6, -2, 2, 4, 16},
-                            {12, -8, 6, 10, 26}};
-        /**
+        if (menu())
+            matrix = usrInput();
+        else
+            matrix = txtInput();
 
+        /*/
+        matrix = new double[][]{{3, -13, 9, 3, -19},
+                {-6, 4, 1, -18, -34},
+                {6, -2, 2, 4, 16},
+                {12, -8, 6, 10, 26}};
+        /*
         double[][] matrix = {{2, 3, 0, 8},
                             {-1, 2, -1, 0},
                             {3, 0, 2, 9}};
-        **/
+        */
         Stack<Integer> rowsDone = new Stack<>();
-        ArrayList<Double> valuesOfX = new ArrayList<>();
+        ArrayList<Double> valuesOfX = new ArrayList<>(matrix.length);
 
         for (double[] y: matrix) {
             for (double x: y)
-                System.out.printf("%.2f ", x);
+                System.out.printf("%10.2f ", x);
             System.out.println();
         }
         double[] highest = getHighestInRow(matrix);
@@ -40,7 +48,7 @@ public class LinearEquations {
         }
         System.out.println();
         for (int i = 0; i < matrix.length; i++) {
-            System.out.printf("x%d = %.2f\n", i+1, valuesOfX.get(matrix.length-(i+1)));
+            System.out.printf("x%d = %5.2f\n", i + 1, valuesOfX.get(matrix.length - (i + 1)));
         }
     }
 
@@ -110,7 +118,6 @@ public class LinearEquations {
 
         for (int columnStart = matrix[row].length - 2; columnStart > column; columnStart--) {
             matrix[row][matrix[row].length-1] -= matrix[row][columnStart] * valuesOfX.get(counter);
-            matrix[row][columnStart] = 0;
             counter++;
         }
         matrix[row][matrix[row].length-1] /= matrix[row][column];
@@ -119,23 +126,85 @@ public class LinearEquations {
 
     public static void multiplierOps(int columnStart, double[][] matrix, Stack<Integer> rowsDone, double[] highestPerRow) {
 
-        int column = columnStart;
         int pivotRow = getPivotRow(columnStart, matrix, rowsDone, highestPerRow);
         for (int row = 0; row < matrix.length; row++) {
             if (rowsDone.contains(row))
                 continue;
 
             double multiplier = getMultiplier(columnStart, row, matrix, pivotRow);
-            for (;column < matrix[row].length; column++) {
+            for (int column = columnStart;column < matrix[row].length; column++) {
                 matrix[row][column] -= matrix[pivotRow][column] * multiplier;
             }
-            column = columnStart;
         }
 
         for(double[] dd : matrix) {
             for (double d : dd)
-                System.out.printf("%.2f ", d);
+                System.out.printf("%10.2f ", d);
             System.out.println();
         }
+    }
+
+    public static boolean menu() {
+        Scanner kbInput = new Scanner(System.in);
+        System.out.println("Welcome to Gaussian Elimination with Scaled Partial Pivoting!!");
+        char c;
+        while (true) {
+            System.out.println("1) User inputted Equations\n" +
+                    "2) Equations from txt file");
+            c = kbInput.nextLine().charAt(0);
+            if (c == '1' || c == '2')
+                break;
+            System.out.println("Invalid input");
+        }
+
+        return c == '1';
+    }
+
+    public static double[][] usrInput() {
+        Scanner kbInput = new Scanner(System.in);
+        int n;
+        try {
+            System.out.println("Input the amount of rows\n(Max is 10)");
+            n = Integer.parseInt(kbInput.nextLine());
+            if (n < 1 || n > 10)
+                throw new NumberFormatException();
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input");
+            return usrInput();
+        }
+        double[][] matrix = new double[n][n+1];
+        for (int y = 0; y < matrix.length; y++) {
+                try {
+                    System.out.printf("""
+                            Input matrix at row %d
+                            (space every entry with a space)
+                            (Each row should have %d entries)
+                            """, y, n+1);
+                    String[] s = kbInput.nextLine().split(" ");
+                    for (int x = 0; x < matrix[y].length; x++) {
+                        matrix[y][x] = Double.parseDouble(s[x]);
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid Format");
+                    return usrInput();
+                }
+        }
+        return matrix;
+    }
+
+    public static double[][] txtInput() {
+        Scanner kbInput = new Scanner(System.in);
+        File file;
+        Scanner fileReader;
+        try {
+            System.out.println("Input the name of the txt file");
+            file = new File(kbInput.nextLine());
+            fileReader = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("Invalid file name");
+            return txtInput();
+        }
+
+        return  new double[10][11];
     }
 }
